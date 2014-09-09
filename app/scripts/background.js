a08ed06c0d0094e090ee362d6f1d4e515d968885
Stepan Mandryka <stepan.mandryka@eleks.com>
2014-09-09 16:58:46 +0300

    Enabled only on ex.ua pages, added some markup

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -4,8 +4,11 @@ chrome.runtime.onInstalled.addListener(function (details) {
     console.log('previousVersion', details.previousVersion);
 });
 
-chrome.tabs.onUpdated.addListener(function (tabId) {
-    chrome.pageAction.show(tabId);
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+	// Show popup only on ex.ua website
+	if(tab.url.match(/^http:\/\/www\.ex\.ua/i)){
+	    chrome.pageAction.show(tabId);
+	}
 });
 
 console.log('\'Allo \'Allo! Event Page for Page Action');
